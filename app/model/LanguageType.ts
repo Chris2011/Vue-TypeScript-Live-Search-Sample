bef0fb813cf171589feffceaf469d344212b1aa6
Chris2011 <christian.lenz@gmx.net>
2017-08-18 14:50:38 +0200

    Changed code.

--- app/model/LanguageType.ts
+++ app/model/LanguageType.ts
@@ -9,10 +9,14 @@ export class LanguageType {
     private isPluginRequired: boolean;
 
     constructor(languageName: string, fileExt: string, isPluginRequired: boolean) {
-        this.icon = fileExt;
+        this.icon = fileExt || languageName;
         this.languageName = languageName;
-        this.fileExt = fileExt;
+        this.fileExt = fileExt || languageName;
         this.isPluginRequired = isPluginRequired;
+    }
+
+    public get Icon() {
+        return this.icon.toLowerCase();
     }
 
     public get LanguageName(): string {
@@ -21,5 +25,13 @@ export class LanguageType {
 
     public set LanguageName(value: string) {
         this.languageName = value;
+    }
+
+    public get FileExt(): string {
+        return this.fileExt.toLowerCase();
+    }
+
+    public setExt(languageType: LanguageType): void {
+        console.log(`Chosen extension - ${languageType.FileExt}`);
     }
 }
