5147e48a8585cfb0d85144110dccbc9a1b703b58
Chris2011 <christian.lenz@gmx.net>
2017-11-15 21:20:44 +0100

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin'),
-        SpriteLoaderPlugin = require('svg-sprite-loader/plugin'),
-        path = require('path'),
-        ExtractTextPlugin = require('extract-text-webpack-plugin');
+SpriteLoaderPlugin = require('svg-sprite-loader/plugin'),
+path = require('path');
 
 module.exports = {
-    entry: ["./app/main", './app/main.scss'],
+    entry: './app/app.ts',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         publicPath: '/generated/',
-        filename: "app.js"
-    },
-    resolve: {
-        // Add '.ts' as a resolvable extension.
-        extensions: ['.ts', '.js']
+        filename: 'app.js'
     },
     module: {
         rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                        ts: 'babel-loader!ts-loader'
+                    }
+                    // other vue-loader options go here
+                }
+            }, {
                 // all files with a '.ts' extension will be handled by 'ts-loader'
-                test: /\.ts$/, loader: ['babel-loader', 'ts-loader']
-            }, {
-                // regular css files
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    use: 'css-loader?importLoaders=1'
-                })
-            }, {
-                // https://jonathanmh.com/webpack-sass-scss-compiling-separate-file/
-                test: /\.scss$/, loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
             }, {
                 // https://github.com/kisenka/svg-sprite-loader
                 test: /\.svg$/,
@@ -38,16 +42,16 @@ module.exports = {
                 }
             }]
     },
+    resolve: {
+        // Add '.ts' as a resolvable extension.
+        extensions: ['.ts', '.js', '.vue', '.json']
+    },
     devtool: 'source-map',
     plugins: [
         new CopyWebpackPlugin([{
                 from: 'app/index.html',
                 to: 'index.html'
             }]),
-        new ExtractTextPlugin({
-            filename: '[name].css',
-            allChunks: true
-        }),
         new SpriteLoaderPlugin()
     ],
     devServer: {
