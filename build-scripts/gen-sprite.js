41c0e8b0566b8ff98769722b88e7e3621982f84e
Chris2011 <christian.lenz@gmx.net>
2018-05-25 15:53:48 +0200

    Renamed folder.

---
+++ build-scripts/gen-sprite.js
@@ -0,0 +1,21 @@
+const nsg = require('node-sprite-generator');
+
+nsg({
+    src: [
+        '.tmp/icons/*.png'
+    ],
+    compositor: 'jimp',
+    stylesheet: 'prefixed-css',
+    spritePath: 'dist/sprite.png',
+    stylesheetPath: '.tmp/sprite.css',
+    stylesheetOptions: {
+        prefix: 'svg-'
+    }
+}, (err) => {
+    if(err) {
+        console.log('Error occured: \n');
+        console.log(err);
+    } else {
+        console.log('Sprite generated!');
+    }
+});
